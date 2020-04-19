456f53616d04dece513d608e7b4cb9a5253be52f
Ismael-UTN <ismael.boff@alumnos.frm.utn.edu.ar>
2020-04-19 17:37:39 -0300

    fixedEj5

--- Programacion II/DiagramasUML/src/Ejercicio5/Factura.java
+++ Programacion II/DiagramasUML/src/Ejercicio5/Factura.java
@@ -1,5 +1,7 @@
 package Ejercicio5;
 
+import java.util.List;
+
 /**
  *
  * @author SkylakeFrost
@@ -8,13 +10,19 @@ public class Factura extends Comprobante {
 
     private float total;
     private Cliente cliente;
-    private Producto producto;
+    private List<Producto> productos;
 
-    public Factura(float total, Cliente cliente, Producto producto, char tipo, int numero, Fecha fecha) {
+    public Factura(float total, Cliente cliente, List<Producto> productos, char tipo, int numero, Fecha fecha) {
         super(tipo, numero, fecha);
         this.total = total;
         this.cliente = cliente;
-        this.producto = producto;
+        this.productos = productos;
+    }
+
+    public Factura(float total, Cliente cliente, List<Producto> productos) {
+        this.total = total;
+        this.cliente = cliente;
+        this.productos = productos;
     }
 
     public Factura(float total) {
@@ -40,12 +48,12 @@ public class Factura extends Comprobante {
         this.cliente = cliente;
     }
 
-    public Producto getProducto() {
-        return producto;
+    public List<Producto> getProductos() {
+        return productos;
     }
 
-    public void setProducto(Producto producto) {
-        this.producto = producto;
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
 
 }
--- Programacion II/DiagramasUML/src/Ejercicio5/main.java
+++ Programacion II/DiagramasUML/src/Ejercicio5/main.java
@@ -1,5 +1,8 @@
 package Ejercicio5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author SkylakeFrost
@@ -8,8 +11,14 @@ public class main {
 
     public static void main(String[] args) {
         //Monto, Cliente, Producto, Tipo Factura, Codigo, Fecha
-        Factura factura = new Factura(232, new Cliente(1, "Comercial"), 
-        new Producto(12, "Cereal", 145), 'A', 2323, new Fecha(12, 10, 2020));
+        List<Producto> productos = new ArrayList<>();
+
+        productos.add(new Producto(1, "Yogurt", 60));
+        productos.add(new Producto(2, "Cereal", 80));
+        productos.add(new Producto(3, "Cafe", 230));
+        productos.add(new Producto(3, "Chocolate", 300));
+
+        Factura factura = new Factura(231, new Cliente(1, "morfar"), productos, 'a', 0, new Fecha(1, 0, 3450));
 
         System.out.println("----------------------------------------------");
 
@@ -18,11 +27,18 @@ public class main {
         System.out.println("Codigo : " + factura.getNumero());
         System.out.println("Fecha : " + factura.getFecha().getDia() + "/" + factura.getFecha().getMes() + "/" + factura.getFecha().getAnio());
         System.out.println("----------------------------------------------");
-        System.out.println("Producto : " + factura.getProducto().getDescripcion());
-        System.out.println("Precio : " + factura.getProducto().getPrecio() + " AR$");
+        int total = 0;
+        System.out.println("Lista De Productos");
+        for (int i = 0; i < factura.getProductos().size(); i++) {
 
-        factura.setTotal(factura.getProducto().getPrecio());
-        System.out.println("TOTAL : " + factura.getTotal() + " AR$");
+            System.out.println("Nombre : " + factura.getProductos().get(i).getDescripcion() + " | Precio : "
+                    + factura.getProductos().get(i).getPrecio() + " AR$");
+            total += factura.getProductos().get(i).getPrecio();
+
+        }
+        System.out.println("-----------------------------------------------");
+        System.out.println("TOTAL : " + total);
+        System.out.println("-----------------------------------------------");
     }
 
 }
